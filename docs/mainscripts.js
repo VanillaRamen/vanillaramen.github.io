e10e8bd38dc8c7bf64776540499de5772e85926c
Vani <118681769+VanillaRamen@users.noreply.github.com>
2024-04-28 16:36:55 -0500

    initialize mainscripts.js

--- docs/mainscripts.js
+++ docs/mainscripts.js
@@ -1,1 +1,17 @@
+// session button clicker thing
+let counter = 0;
 
+function count() {
+  counter++;
+  // needs to update an html element
+}
+
+// session to-do / reminder list
+let sessionreminders = [];
+
+function setremind(inp) {
+  sessionreminders.push(inp);
+  // needs to update an html element
+}
+
+// figure out how to make a timer as well
