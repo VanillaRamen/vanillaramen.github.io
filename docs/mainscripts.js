d26dd4f65a85d39df0dfa7322f95203bdc0d9b96
Vani <118681769+VanillaRamen@users.noreply.github.com>
2024-05-02 20:32:54 -0500

    Update mainscripts.js

--- docs/mainscripts.js
+++ docs/mainscripts.js
@@ -3,18 +3,24 @@
 // 🐟🐠🐡
 
 let regfish = document.getElementById('regfish');
+console.log(document.getElementById('regfish'));
 let tropfish = document.getElementById('tropfish');
+console.log(document.getElementById('tropfish'));
 let puffers = document.getElementById('puffers');
+console.log(document.getElementById('puffers'));
 
 function fish() {
   if (Math.round(Math.random())) {
     console.log('🐟');
     regfish++;
+    console.log(regfish);
   } else if (Math.round(Math.random())) {
     console.log('🐠');
     tropfish++;
+    console.log(tropfish);
   } else if (Math.round(Math.random())) {
     console.log('🐡');
     puffers++;
+    console.log(puffers);
   } else { return fish() };
 } // preliminary
