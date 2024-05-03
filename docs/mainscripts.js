19cac73a542181e1019b1ab3357e7bbca1304efe
Vani <118681769+VanillaRamen@users.noreply.github.com>
2024-05-02 20:31:04 -0500

    Update mainscripts.js

--- docs/mainscripts.js
+++ docs/mainscripts.js
@@ -2,12 +2,19 @@
 
 // 🐟🐠🐡
 
+let regfish = document.getElementById('regfish');
+let tropfish = document.getElementById('tropfish');
+let puffers = document.getElementById('puffers');
+
 function fish() {
   if (Math.round(Math.random())) {
     console.log('🐟');
+    regfish++;
   } else if (Math.round(Math.random())) {
     console.log('🐠');
+    tropfish++;
   } else if (Math.round(Math.random())) {
     console.log('🐡');
+    puffers++;
   } else { return fish() };
 } // preliminary
