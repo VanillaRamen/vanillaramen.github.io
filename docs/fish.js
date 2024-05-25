5937b45de11adf0b02fe98f5a152cb4ec3753484
Vani <118681769+VanillaRamen@users.noreply.github.com>
2024-05-25 00:52:46 -0500

    Update fish.js

--- docs/fish.js
+++ docs/fish.js
@@ -16,12 +16,12 @@ let ids = ['regfish', 'tropfish', 'puffers', 'squids', 'octopuses', 'dragons']; 
 const names = ['regular fish 🐟', 'tropical fish 🐠', 'pufferfish 🐡', 'squid 🦑', 'octopus 🐙', 'dragon 🐉']; // for 'you caught a...' message
 
 function fish() { // fishing function
-     let num = Math.floor(Math.random() * ids.length);
+     let num = Math.floor(Math.random() * names.length);
      fishies[ids[num]]++;
      
-     console.log(ids[num]); // debug
+     console.log(names[num]); // debug
 
      document.getElementById(ids[num]).textContent = fishies[ids[num]]; // sets the HTML & updates the page
 
-     document.getElementById('youcaught').textContent = 'You caught a' + names[num] + '!'; // tells you what you caught!
-} // beta code left below just in case... pls ignore
+     document.getElementById('youcaught').textContent = 'You caught a ' + names[num] + '!'; // tells you what you caught!
+} // i genuinely do not know what is wrong here
